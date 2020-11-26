2cf596a75d144b3deb8530c4c83b542c7010fee6
杜润生 <bestriven666@outlook.com>
2020-11-26 15:39:24 +0800

    echarts-js

---
+++ echarts/js/Pie.js
@@ -0,0 +1,98 @@
+﻿var PieChart = echarts.init(document.getElementById('Pie'));
+option = {
+    tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b}: {c} ({d}%)'
+    },
+    legend: {
+        orient: 'vertical',
+        left: 1,
+        data: ['直达', '营销广告', '搜索引擎', '邮件营销', '联盟广告', '视频广告', '百度', '谷歌', '必应', '其他']
+    },
+    series: [
+        {
+            name: '访问来源',
+            type: 'pie',
+            center:['50%','50%'],
+            selectedMode: 'single',
+            
+            radius: [0, '21%'],
+
+            label: {
+                position: 'inner'
+            },
+            labelLine: {
+                show: false
+            },
+            data: [
+                {value: 335, name: '直达', selected: true},
+                {value: 679, name: '营销广告'},
+                {value: 1548, name: '搜索引擎'}
+            ],
+          width:'auto'
+        },
+        {
+            name: '访问来源', //整体的框
+            type: 'pie',
+            center:['50%','50%'],
+            radius: ['28%', '38.5%'],
+            label: {
+                formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
+                backgroundColor: '#eee',
+                borderColor: '#aaa',
+                borderWidth: 1,
+                borderRadius: 4,
+                // shadowBlur:3,
+                // shadowOffsetX: 2,
+                // shadowOffsetY: 2,
+                // shadowColor: '#999',
+                // padding: [0, 7],
+                rich: {
+                    a: {
+                       //最上层的大小
+                        color: '#999',
+                        lineHeight: 11,
+                        fontSize:8,
+                        width:'60%',
+                        align: 'center'
+                    },
+                    hr: {  //中间的线
+                        borderColor: '#aaa',
+                        width: '100%',
+                        borderWidth: 0.5,
+                        height: 0
+                    },
+                    b: {
+                        //提示名,谷歌等的字体框
+                        fontSize: 8,
+                        lineHeight: 18
+                    },
+                    per: {//小边框
+                        width:'50%',
+                        left:'70',
+                        fontSize:8,
+                        color: '#eee',
+                        backgroundColor: '#334455',
+                        padding: [2, 4],
+                        borderRadius: 2
+                    }
+                }
+            },
+            data: [
+                {value: 335, name: '直达'},
+                {value: 310, name: '邮件营销'},
+                {value: 234, name: '联盟广告'},
+                {value: 135, name: '视频广告'},
+                {value: 1048, name: '百度'},
+                {value: 251, name: '谷歌'},
+                {value: 147, name: '必应'},
+                {value: 102, name: '其他'}
+            ],
+         width:'auto'
+        }
+    ]
+};
+
+
+PieChart.setOption(option);
+
---
+++ echarts/js/sankey.js
@@ -0,0 +1,438 @@
+﻿var SankChart = echarts.init(document.getElementById('sankey'));
+option = {
+    series: {
+        type: 'sankey',
+        layout: 'none',
+        focusNodeAdjacency: 'allEdges',
+        data: [{
+            name: 'a1'
+        }, {
+            name: 'b1'
+        }, {
+            name: 'c1'
+        }, {
+            name: 'd1'
+        }, {
+            name: 'e1'
+        },
+        {
+            name: 'a2'
+        }, {
+            name: 'b2'
+        }, {
+            name: 'c2'
+        }, {
+            name: 'd2'
+        }, {
+            name: 'e2'
+        },
+        {
+            name: 'a3'
+        }, {
+            name: 'b3'
+        }, {
+            name: 'c3'
+        }, {
+            name: 'd3'
+        }, {
+            name: 'e3'
+        },
+        {
+            name: 'a4'
+        }, {
+            name: 'b4'
+        }, {
+            name: 'c4'
+        }, {
+            name: 'd4'
+        }, {
+            name: 'e4'
+        }
+        ],
+        links: [
+            {
+            source: 'a1',
+            target: 'a2',
+            value: 5
+        }, 
+            {
+            source: 'a1',
+            target: 'b2',
+            value: 5
+        }, 
+        {
+            source: 'a1',
+            target: 'c2',
+            value: 5
+        },
+        {
+            source: 'a1',
+            target: 'd2',
+            value: 5
+        },
+        {
+            source: 'a1',
+            target: 'e2',
+            value: 3
+        }, {
+            source: 'b1',
+            target: 'a2',
+            value: 8
+        },
+        {
+            source: 'b1',
+            target: 'b2',
+            value: 8
+        },
+        {
+            source: 'b1',
+            target: 'c2',
+            value: 8
+        },
+        {
+            source: 'b1',
+            target: 'd2',
+            value: 8
+        },
+        {
+            source: 'b1',
+            target: 'e2',
+            value: 5
+        }, 
+        {
+            source: 'c1',
+            target: 'a2',
+            value: 5
+        }, 
+        {
+            source: 'c1',
+            target: 'b2',
+            value: 5
+        }, 
+        {
+            source: 'c1',
+            target: 'c2',
+            value: 5
+        }, 
+        {
+            source: 'c1',
+            target: 'd2',
+            value: 5
+        }, 
+        {
+            source: 'c1',
+            target: 'e2',
+            value: 5
+        }, 
+        {
+            source: 'd1',
+            target: 'a2',
+            value: 5
+        },
+        {
+            source: 'd1',
+            target: 'b2',
+            value: 5
+        },
+        {
+            source: 'd1',
+            target: 'c2',
+            value: 5
+        },
+        {
+            source: 'd1',
+            target: 'd2',
+            value: 5
+        },
+        {
+            source: 'd1',
+            target: 'e2',
+            value: 5
+        },
+        {
+            source: 'e1',
+            target: 'a2',
+            value: 5
+        },
+        {
+            source: 'e1',
+            target: 'b2',
+            value: 5
+        },
+        {
+            source: 'e1',
+            target: 'c2',
+            value: 5
+        },
+        {
+            source: 'e1',
+            target: 'd2',
+            value: 5
+        },
+        {
+            source: 'e1',
+            target: 'e2',
+            value: 5
+        },
+        
+        //---------------断层分界线 
+        
+        {
+            source: 'a2',
+            target: 'a3',
+            value: 5
+        }, 
+            {
+            source: 'a2',
+            target: 'b3',
+            value: 5
+        }, 
+        {
+            source: 'a2',
+            target: 'c3',
+            value: 5
+        },
+        {
+            source: 'a2',
+            target: 'd3',
+            value: 5
+        },
+        {
+            source: 'a2',
+            target: 'e3',
+            value: 3
+        }, {
+            source: 'b2',
+            target: 'a3',
+            value: 8
+        },
+        {
+            source: 'b2',
+            target: 'b3',
+            value: 8
+        },
+        {
+            source: 'b2',
+            target: 'c3',
+            value: 8
+        },
+        {
+            source: 'b2',
+            target: 'd3',
+            value: 8
+        },
+        {
+            source: 'b2',
+            target: 'e3',
+            value: 5
+        }, 
+        {
+            source: 'c2',
+            target: 'a3',
+            value: 5
+        }, 
+        {
+            source: 'c2',
+            target: 'b3',
+            value: 5
+        }, 
+        {
+            source: 'c2',
+            target: 'c3',
+            value: 5
+        }, 
+        {
+            source: 'c2',
+            target: 'd3',
+            value: 5
+        }, 
+        {
+            source: 'c2',
+            target: 'e3',
+            value: 5
+        }, 
+        {
+            source: 'd2',
+            target: 'a3',
+            value: 5
+        },
+        {
+            source: 'd2',
+            target: 'b3',
+            value: 5
+        },
+        {
+            source: 'd2',
+            target: 'c3',
+            value: 5
+        },
+        {
+            source: 'd2',
+            target: 'd3',
+            value: 5
+        },
+        {
+            source: 'd2',
+            target: 'e3',
+            value: 5
+        },
+        {
+            source: 'e2',
+            target: 'a3',
+            value: 5
+        },
+        {
+            source: 'e2',
+            target: 'b3',
+            value: 5
+        },
+        {
+            source: 'e2',
+            target: 'c3',
+            value: 5
+        },
+        {
+            source: 'e2',
+            target: 'd3',
+            value: 5
+        },
+        {
+            source: 'e2',
+            target: 'e3',
+            value: 5
+        },
+        
+        // ---------------断层分界线
+        
+        {
+            source: 'a3',
+            target: 'a4',
+            value: 5
+        }, 
+            {
+            source: 'a3',
+            target: 'b4',
+            value: 5
+        }, 
+        {
+            source: 'a3',
+            target: 'c4',
+            value: 5
+        },
+        {
+            source: 'a3',
+            target: 'd4',
+            value: 5
+        },
+        {
+            source: 'a3',
+            target: 'e4',
+            value: 3
+        }, {
+            source: 'b3',
+            target: 'a4',
+            value: 8
+        },
+        {
+            source: 'b3',
+            target: 'b4',
+            value: 8
+        },
+        {
+            source: 'b3',
+            target: 'c4',
+            value: 8
+        },
+        {
+            source: 'b3',
+            target: 'd4',
+            value: 8
+        },
+        {
+            source: 'b3',
+            target: 'e4',
+            value: 5
+        }, 
+        {
+            source: 'c3',
+            target: 'a4',
+            value: 5
+        }, 
+        {
+            source: 'c3',
+            target: 'b4',
+            value: 5
+        }, 
+        {
+            source: 'c3',
+            target: 'c4',
+            value: 5
+        }, 
+        {
+            source: 'c3',
+            target: 'd4',
+            value: 5
+        }, 
+        {
+            source: 'c3',
+            target: 'e4',
+            value: 5
+        }, 
+        {
+            source: 'd3',
+            target: 'a4',
+            value: 5
+        },
+        {
+            source: 'd3',
+            target: 'b4',
+            value: 5
+        },
+        {
+            source: 'd3',
+            target: 'c4',
+            value: 5
+        },
+        {
+            source: 'd3',
+            target: 'd4',
+            value: 5
+        },
+        {
+            source: 'd3',
+            target: 'e4',
+            value: 5
+        },
+        {
+            source: 'e3',
+            target: 'a4',
+            value: 5
+        },
+        {
+            source: 'e3',
+            target: 'b4',
+            value: 5
+        },
+        {
+            source: 'e3',
+            target: 'c4',
+            value: 5
+        },
+        {
+            source: 'e3',
+            target: 'd4',
+            value: 5
+        },
+        {
+            source: 'e3',
+            target: 'e4',
+            value: 5
+        },
+        
+        
+         ]
+    }
+};
+
+
+SankChart.setOption(option);

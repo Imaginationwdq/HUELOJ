e8699e74c96f94c3325fb332bfb1fc7a404dca50
王栋卿 <bestriven666@outlook.com>
2020-11-26 15:48:43 +0800

    学生界面、老师界面初始化

--- teacherEcharts.php
+++ teacherEcharts.php
@@ -1,3 +1,19 @@
 <?php
-    echo "老师界面";
+////////////////////////////Common head
+$cache_time=10;
+$OJ_CACHE_SHARE=false;
+require_once('./include/cache_start.php');
+require_once('./include/db_info.inc.php');
+require_once('./include/const.inc.php');
+require_once('./include/memcache.php');
+require_once('./include/setlang.php');
+$view_title= "Welcome To Online Judge";
+$result=false;
+///////////////////////////MAIN
+
+/////////////////////////Template
+require("template/".$OJ_TEMPLATE."/teacherEcharts.php");
+/////////////////////////Common foot
+if(file_exists('./include/cache_end.php'))
+    require_once('./include/cache_end.php');
 ?>

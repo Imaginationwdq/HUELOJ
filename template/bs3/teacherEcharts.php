86ff3ff0cc261055d77a70325d64248e0d151679
杜润生 <bestriven666@outlook.com>
2020-11-30 17:03:41 +0800

    优化布局

--- template/bs3/teacherEcharts.php
+++ template/bs3/teacherEcharts.php
@@ -58,8 +58,8 @@
             <div class="col-md-3" style="height: 400px" id="Scatter">3</div>
         </div>
         <div class="row">
-            <div class="col-md-4" style="height: 400px">4</div>
-            <div class="col-md-4" style="height: 400px ;overflow-y: auto;">
+            <div class="col-md-3" style="height: 400px">4</div>
+            <div class="col-md-5" style="height: 400px ;overflow-y: auto;">
                 <?php echo $student ?>
             </div>
             <div class="col-md-4" style="height: 400px">
